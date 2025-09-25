971e570e5730a42dfddf468a83958fea84a0a304
arifk <aarifkaracaa@gmail.com>
2025-09-25 22:34:09 +0300

    Remove leftover empty/duplicate migrations and keep only relevant migrations

---
+++ UretimAPI/Migrations/20250922154913_PTF_Add_CycleTime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UretimAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PTF_Add_CycleTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CycleTime",
+                table: "ProductionTrackingForms",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CycleTime",
+                table: "ProductionTrackingForms");
+        }
+    }
+}
